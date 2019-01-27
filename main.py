01c34a3855bc53c875aafb85f9c6e1a62165119d
Travis Anderson <thetanderson@gmail.com>
2019-01-26 19:28:35 -0500

    adding in the mainpy as a controller

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
+import twitbot
+import logging
+import time
+import signal
+
+logger = logging.getLogger(__name__)
+exit_flag = False
+
+logger = logging.getLogger(__name__)
+exit_flag = False
+
+
+def signal_handler(sig_num, frame):
+    """Smooth exit from system"""
+    global exit_flag
+    logger.warning(
+        '!!!!!!!!!!-----------!!!!!!!!!!!!!!!!!: {}'.format(str(sig_num)))
+    if sig_num:
+        exit_flag = True
+
+
+def init_logger():
+    logging.basicConfig(
+        format=(
+            '%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s '
+            '[%(threadName) -12s] %(message)s'),
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logger.setLevel(logging.DEBUG)
 
 
 def main():
-    pass
+    global exit_flag
+    init_logger()
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    twitbot.init_logger()
+    tb = twitbot.WatchTwitter()
+    # tb.self
+    tb.init_stream('python')
+    while not exit_flag:
+        # logger.info("hello me")
+        time.sleep(3)
+        tb.pause_stream()
+        time.sleep(3)
+        tb.add_subscription('Trump')
+        time.sleep(2)
+    #     tb.remove_subscription('Trump')
+    # tb.pause_stream()
 
 
 if __name__ == "__main__":
--- twitbot.py
+++ twitbot.py
@@ -8,29 +8,35 @@ This is for contacting twitter, and watching a specific user or word
 
 """
 import logging
-import signal
+# import signal
 import tweepy
 import time
 import os
+# import sys
 import datetime
 from threading import Thread
+import json
+import threading
 
-logger = logging.getLogger(__file__)
+
+logger = logging.getLogger(__name__)
 exit_flag = False
 
 
-def signal_handler(sig_num, frame):
-    """Smooth exit from system"""
-    global exit_flag
-    logger.warning('Received exit signal number: {}'.format(str(sig_num)))
-    if sig_num:
-        exit_flag = True
+# def signal_handler(sig_num, frame):
+#     """Smooth exit from system"""
+#     global exit_flag
+#     logger.warning('Received exit signal number: {}'.format(str(sig_num)))
+#     if sig_num:
+#         exit_flag = True
 
 
 def _start(self, is_async):
+    """Monkey patch to allow multi threading so twitter can run and
+    main program can run"""
     self.running = True
     if is_async:
-        logger.warning("Running Daemon thread")
+        logger.warning("Initiating multithread")
         self._thread = Thread(
             target=self._run, name="Tweepy Thread", daemon=True)
         self._thread.start()
@@ -39,6 +45,7 @@ def _start(self, is_async):
 
 
 class WatchTwitter(tweepy.StreamListener):
+    """Class that subscribes to keywords on twitter """
 
     def __init__(self):
         logger.info("Creating api")
@@ -50,19 +57,81 @@ class WatchTwitter(tweepy.StreamListener):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
-        logger.warning("Did a monkey patch, could use a demo on this")
+        # logger.warning("Did a monkey patch, could use a demo on this")
         tweepy.Stream._start = _start
+        self.subscriptions = []
+        self._stop_event = threading.Event()
+        self.stream_timestamp = 0
+        self.master_timestamp = 0
 
-    def start_stream(self, find_string):
-        logger.info('Found string: {}'.format(find_string))
+    def add_subscription(self, subscribe_to):
+        """If stream is running adds new subscription, restarts stream"""
+        if subscribe_to not in self.subscriptions:
+            logger.info('Adding subscription: {}'.format(subscribe_to))
+            self.subscriptions.append(subscribe_to)
+            logger.info(self.subscriptions)
+            self.stream.running = False
+            self.start_stream()
+        else:
+            logger.info("Already subscribed: {}" .format(self.subscriptions))
+
+    def remove_subscription(self, unsubscribe_from):
+        logger.info("Attempting to remove {}".format(unsubscribe_from))
+        if unsubscribe_from in self.subscriptions:
+            logger.info(
+                'Removing from subscriptions: {}'.format(unsubscribe_from))
+            self.subscriptions.remove(unsubscribe_from)
+            self.stream.running = False
+            self.start_stream()
+
+    def pause_stream(self):
+        if self.stream.running:
+            logger.info("Pausing all subscriptions: {}".format(
+                self.subscriptions))
+            self.stream.running = False
+
+    def restart_stream(self):
+        if not self.stream.running:
+            logger.info("Restarting stream")
+            self.start_stream()
+
+    def init_stream(self, string):
+        self.subscriptions.append(string)
+        self.start_stream()
+
+    def exit_flag_toggle(self):
+        global exit_flag
+        if exit_flag:
+            self.pause_stream()
+
+    def start_stream(self):
+        global exit_flag
+        exit_flag = False
+        logger.info('Subscriptions: {}'.format(self.subscriptions))
         self.stream = tweepy.Stream(auth=self.api.auth, listener=self)
-        self.stream.filter(track=[find_string], is_async=True)
+        time.sleep(1)
+        self.stream.filter(track=self.subscriptions, is_async=True)
 
     def on_status(self, status):
         logger.info(status.text)
 
     def on_connect(self):
-        logger.info('Connected at: {}'.format(datetime.datetime.now()))
+        self.stream_timestamp = datetime.datetime.now()
+        logger.info('Connected to twitter at: {}'.format(
+            datetime.datetime.now()))
+        if not self.master_timestamp:
+            self.master_timestamp = self.stream_timestamp
+
+    def get_user_timeline(self, users_name):
+        return self.api.user_timeline(screen_name=users_name, count=1)
+
+    def most_recent_tweet(self, users_name):
+        logger.info('subscriptions:')
+        user = self.get_user_timeline(users_name)
+        status = user[0]._json
+        json_str = json.dumps(status)
+        json_dict = json.loads(json_str)
+        logger.info('{} tweeted: {}'.format(users_name, json_dict['text']))
 
 
 def log_config():
@@ -79,17 +148,35 @@ def log_set_level():
     logger.setLevel(logging.DEBUG)
 
 
+def init_logger():
+    logging.basicConfig(
+        format=(
+            '%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s '
+            '[%(threadName) -12s] %(message)s'),
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logger.setLevel(logging.DEBUG)
+
+    # signal.signal(signal.SIGINT, signal_handler)
+    # signal.signal(signal.SIGTERM, signal_handler)
+
+
 def main():
+    global exit_flag
     log_config()
     log_set_level()
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
+    # signal.signal(signal.SIGINT, signal_handler)
+    # signal.signal(signal.SIGTERM, signal_handler)
 
     tb = WatchTwitter()
-    tb.start_stream('python')
+    tb.init_stream('python')
     while not exit_flag:
-        logger.info("Hello world")
-        time.sleep(1)
+        time.sleep(5)
+        tb.pause_stream()
+        time.sleep(5)
+        tb.add_subscription('Trump')
+        time.sleep(5)
+        tb.remove_subscription('Trump')
 
 
 if __name__ == "__main__":
